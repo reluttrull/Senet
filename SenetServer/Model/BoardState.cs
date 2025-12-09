5149c9ca75ad9ca09d362d9b7d30b57e54d9e3c2
Ryan Luttrull <reluttrull@gmail.com>
2025-12-09 09:56:56 -0600

    Adding game logic: update list of pawn indices that can move.  Will need to update on every roll, every move, and every turn change.

--- SenetServer/Model/BoardState.cs
+++ SenetServer/Model/BoardState.cs
@@ -4,6 +4,7 @@
     {
         public List<int> WhitePositions { get; set; }
         public List<int> BlackPositions { get; set; }
+        public List<int> MovablePositions { get; set; }
         public List<int> Sticks { get; set; } = new List<int>();
         public int SticksValue { get; set; }
         public bool IsWhiteTurn { get; set; } = true;
@@ -12,6 +13,12 @@
             WhitePositions = new List<int>() { 0, 2, 4, 6, 8 };
             BlackPositions = new List<int>() { 1, 3, 5, 7, 9 };
             RollSticks();
+        }
+        public void SetCanMove()
+        {
+            MovablePositions = (IsWhiteTurn ? WhitePositions : BlackPositions)
+                .Where(pawn => PawnCanMove(pawn))
+                .ToList();
         }
         public void RollSticks()
         {
@@ -22,9 +29,50 @@
                 Sticks.Add(random.Next(2));
             }
             SticksValue = GetSticksValue();
+            SetCanMove();
         }
 
-        public int GetSticksValue()
+        private bool PawnCanMove(int index)
+        {
+            List<int> sameColor = IsWhiteTurn ? WhitePositions : BlackPositions;
+            List<int> differentColor = IsWhiteTurn ? BlackPositions : WhitePositions;
+            List<int> safeSquares = [14, 25, 27, 28];
+            int targetLocation = index + SticksValue;
+
+            if (IsEnemyGuarded(differentColor, targetLocation)) return false; // can't swap guarded pawns
+            if (safeSquares.Contains(targetLocation)) return false; // can't capture on safe squares
+            if (IsEnemyBlockaded(differentColor, targetLocation)) return false; // can't pass blockade
+            if (sameColor.Contains(targetLocation) && targetLocation < 30) return false; // can't oust own pawn
+            if (index == 25 || (index == 29 && targetLocation > 29)) return true; // home free from 25
+            if (targetLocation > 25 && targetLocation < 30 && index != 25) return false; // did not pass go
+            if ((index == 27 || index == 28) && targetLocation != 30) return false; // need to roll exactly 3 and 2, respectively
+            return true; // otherwise, all good
+        }
+
+        private bool IsEnemyGuarded(List<int> enemyPieces, int targetIndex)
+        {
+            if (!enemyPieces.Contains(targetIndex)) return false;
+            // two or more pieces next to each other can't be captured
+            // ...just double-check we're not including pieces past the finish line
+            if ((enemyPieces.Contains(targetIndex + 1) && targetIndex < 29) 
+                || enemyPieces.Contains(targetIndex - 1)) return true;
+            return false;
+        }
+
+        private bool IsEnemyBlockaded(List<int> enemyPieces, int targetIndex)
+        {
+            if (!enemyPieces.Contains(targetIndex)) return false;
+            // from start to end index, blockade can either look like:
+            //  sxxx_e  or s_xxxe
+            if (SticksValue > 3
+                && enemyPieces.Contains(targetIndex - 2)
+                && enemyPieces.Contains(targetIndex - 3)
+                && (enemyPieces.Contains(targetIndex - 1) || enemyPieces.Contains(targetIndex - 4))) return true;
+            return false;
+
+        }
+
+        private int GetSticksValue()
         {
             switch (Sticks.Count(n => n == 1))
             {

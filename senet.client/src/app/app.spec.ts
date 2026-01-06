ca4901e20b4d11938f16fd2598116f7a9cb33f3c
Ryan Luttrull <reluttrull@gmail.com>
2026-01-06 09:55:36 -0600

    Update workflow: should deploy to Pages /Senet/ on client folder change

--- senet.client/src/app/app.spec.ts
+++ senet.client/src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, senet-client');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Senet');
   });
 });
